f0de8ba903dbcd0a4752a73c7c6ad8e2526ae65d
Bahdan Piatrouski <82778510+kefaxoo@users.noreply.github.com>
2024-06-06 01:09:44 +0300

    Initial Commit

---
+++ effective-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  effective-test
+//
+//  Created by Bahdan Piatrouski on 6.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ effective-test/effective_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  effective_testApp.swift
+//  effective-test
+//
+//  Created by Bahdan Piatrouski on 6.06.24.
+//
+
+import SwiftUI
+
+@main
+struct effective_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
